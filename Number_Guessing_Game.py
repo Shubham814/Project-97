2486768a00c3d2bf202ddf6085c01aaa7689c2e2
Shubham1618 <76488872+Shubham814@users.noreply.github.com>
2022-01-03 08:42:32 -0500

    Update Number_Guessing_Game.py

--- Number_Guessing_Game.py
+++ Number_Guessing_Game.py
@@ -6,22 +6,28 @@ chances = 0
 
 print("___Welcome to the Number Guessing Game___")
 
-while chances<6:
+while chances<5:
     chances+=1
     guess = int(input("Enter a guess for the number (between 1 to 9) : "))
     if guess>number:
         if chances < 5:
             print("Your Guess is larger than the number. Enter a number smaller than" , guess )
+            print("                         ")
         else:
-            print("___You LOSE! You have attempted 5 chances___")
+            print("___You LOSE! The number is", number)
+            print("                 ")
     elif guess<number:
         if chances < 5:
             print("Your Guess is smaller than the number. Enter a number larger than" , guess)
+            print("                         ")
         else:
-            print("___You LOSE! You have attempted 5 chances___")
+            print("___You LOSE! The number is", number)
+            print("                 ")
     else:
         if chances < 5:
             print("___Congratulations! You Got the right number___")
+            print("                         ")
             chances = 10
         else:
-            print("___You LOSE! You have attempted 5 chances___")
+            print("You LOSE! The number is", number)
+            print("                 ")
